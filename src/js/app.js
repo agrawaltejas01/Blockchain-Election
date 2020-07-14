cd7ca1d20e883e98c12028904efd3cf1999eff92
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-07-14 11:18:41 +0530

    Basic client side application created

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,164 @@
-App = {
-  web3Provider: null,
-  contracts: {},
+// Provider is how web3 talks to blockchain
+// they are basically websites running geth or parity nodes that talk to ethereum
+App =
+{
+	web3Provider: null,
+	contracts: {},
+	account: '0x0',
+	hasVotes: false,
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+	init: function () {
+		return App.initWeb3();
+	},
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+	// initialize connection of client side application to our local blockchain
+	initWeb3: function () {
+		// We get an instance of web3 attached to windows provided by Meta Mask
+		// Meta Masks turns chrome into blockchain browser
+		// (a browser that can connect to Etherium n/w)
+		if (typeof web3 != 'undefined') {
+			// When we login with meta Mask, it gives us a web3 provider
+			App.web3Provider = web3.currentProvider;
+			// set that web3 provider to our applicatrion web3 provider
+			web3 = new Web3(web3.currentProvider);
+		}
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+		// If no instance of web3 provider is provided
+		else {
+			// set web3 provider of application to our local blockchain instance
+			App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+			web3 = new Web3(App.web3Provider);
+		}
 
-    return await App.initWeb3();
-  },
+		return App.initContract();
+	},
 
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
+	initContract: function () {
 
-    return App.initContract();
-  },
+		// Elecition.json is read from bs-config.json
+		// In there directory is given as build/contracts
+		// In there is , Election.json 
+		$.getJSON("Election.json", function(election) {
+			// Instatiate a new truffle contract from the artifact
+			App.contracts.Election = TruffleContract(election);
 
-  initContract: function() {
-    /*
-     * Replace me...
-     */
+			// Connect provider to interact with contract
+			App.contracts.Election.setProvider(App.web3Provider);
 
-    return App.bindEvents();
-  },
+			return App.render();
+		});		
+	},
+	
 
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
+	render: function () 
+	{
+		var electionInstance;
+		var loader = $('#loader');
+		var content = $('#content');
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
+		// This is asyn function
+		// So we are going to need to show and hide loader and content at appropriate places
 
-  handleAdopt: function(event) {
-    event.preventDefault();
+		loader.show();
+		content.hide();
 
-    var petId = parseInt($(event.target).data('id'));
+		// Load user account data
+		if(web3.currentProvider.enable){
+			//For metamask
+			web3.currentProvider.enable().then(function(acc){
+				App.account = acc[0];
+				$("#accountAddress").html("Your Account: " + App.account);
+			});
+		} else{
+			App.account = web3.eth.accounts[0];
+			$("#accountAddress").html("Your Account: " + App.account);
+		}
 
-    /*
-     * Replace me...
-     */
-  }
+		// Load contract data
+		App.contracts.Election.deployed().then(function(instance) {
+			electionInstance = instance;
+			return electionInstance.candidatesCount();
+		}).then(function(candidatesCount) {
 
+			var candidatesResult = $("#candidatesResult");
+			candidatesResult.empty();
+
+			for (let i = 1; i <= candidatesCount; i++) {
+				electionInstance.candidates(i).then(function (candidate) {
+					var id = candidate[0];
+					var name = candidate[1];
+					var voteCount = candidate[2];
+
+					// Render Candidate Result
+					var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>";
+					candidatesResult.append(candidateTemplate);
+				});				
+			}
+
+			loader.hide();
+			content.show();
+		}).catch(function(error) {
+			console.log(error);
+		});
+	}
+	// render: function() {
+	// 	var electionInstance;
+	// 	var loader = $("#loader");
+	// 	var content = $("#content");
+	
+	// 	loader.show();
+	// 	content.hide();
+	
+	// 	// Load account data
+	// 	web3.eth.getCoinbase(function(err, account) {
+	// 	  if (err === null) {
+	// 		App.account = account;
+	// 		$("#accountAddress").html("Your Account: " + account);
+	// 	  }
+	// 	});
+	
+	// 	// Load contract data
+	// 	App.contracts.Election.deployed().then(function(instance) {
+	// 	  electionInstance = instance;
+	// 	  return electionInstance.candidatesCount();
+	// 	}).then(function(candidatesCount) {
+	// 	  var candidatesResults = $("#candidatesResults");
+	// 	  candidatesResults.empty();
+	
+	// 	  var candidatesSelect = $('#candidatesSelect');
+	// 	  candidatesSelect.empty();
+	
+	// 	  for (var i = 1; i <= candidatesCount; i++) {
+	// 		electionInstance.candidates(i).then(function(candidate) {
+	// 		  var id = candidate[0];
+	// 		  var name = candidate[1];
+	// 		  var voteCount = candidate[2];
+	
+	// 		  // Render candidate Result
+	// 		  var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+	// 		  candidatesResults.append(candidateTemplate);
+	
+	// 		  // Render candidate ballot option
+	// 		  var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+	// 		  candidatesSelect.append(candidateOption);
+	// 		});
+	// 	  }
+	// 	  return electionInstance.voters(App.account);
+	// 	}).then(function(hasVoted) {
+	// 	  // Do not allow a user to vote
+	// 	  if(hasVoted) {
+	// 		$('form').hide();
+	// 	  }
+	// 	  loader.hide();
+	// 	  content.show();
+	// 	}).catch(function(error) {
+	// 	  console.warn(error);
+	// 	});
+	//   },
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+$(function () {
+	$(window).load(function () {
+		App.init();
+	});
 });
