bf4ef511b12efecd64dd5a23d9745f8f471e65c9
agrawaltejas01 <agrawaltejas01@gmail.com>
2020-07-14 12:47:17 +0530

    voting functionallity added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -29,15 +29,19 @@ contract Election
 
     // Array of Candidates
     // Created by using mapping
-
     // Store and Fetch candidates
     mapping(uint => Candidate) public candidates;
 
     // mapping does not tell length
     // Also does not allow to iterate over variable
     // Hence a variable for keeping count of that
-
     uint public candidatesCount;
+
+    // We need to keep check that a user can only vote once
+    // For that we need to keep a mapping of user
+    // If user address is not in mapping, map will return false(default), telling user has not voted
+    mapping(address => bool) public voters;
+
     constructor() public
     {
         addCandidates("Candidate 1");
@@ -58,4 +62,31 @@ contract Election
     // candidate['id'].toNumber
     // candidate['name']
 
+
+    // Solidity allows to pass meta data to function along with paramertes
+    // vote(1 { from : .... }) => {} = metadata
+    // from => address that has called vote function
+    // from is then converted into msg.sender
+    function vote (uint _candidateId) public
+    {
+        // valid candidateId
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "candidateId is not valid");
+
+        // require that voter has not already voted
+        require(!voters[msg.sender], "Voter has already voted");
+
+        // record that user has voted
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+
+    // Election.deployed().then(function(i) { app = i;})
+    // web3.eth.getAccounts().then(function(acc) {account = acc } ) => Gives list of all availabel account
+    // account[0]
+    // app.vote(1, { from : account[0] }) => Success
+    // app.vote(1, { from : account[0] }) => Fail (reason: 'Voter has already voted', )
+    // app.vote(1, { from : account[2] }) => Success
+    // app.vote(1, { from : account[5] }) => Success
+    // app.vote(1, { from : account[5] }) => Fail (reason: 'Voter has already voted', )
+    // app.vote(6, { from : account[3] } ) => Fail (reason: 'candidateId is not valid', )
 }
--- test/election.js
+++ test/election.js
@@ -1,6 +1,6 @@
 var Election = artifacts.require("./Election.sol");
 
-contract("Election", function(account){
+contract("Election", function(accounts){
 
     // Check no if candidates
     it("Initializes with 2 candidates", function(){
@@ -32,4 +32,64 @@ contract("Election", function(account){
             assert.equal(candidate['voteCount'], 0, 'incorrect voteCount 2');
         });
     });
+
+    it('a voter can vote', function(){
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, { from : accounts[0] });
+            // vote function will return a recipet
+        }).then( function(recipt) {
+            return electionInstance.voters(accounts[0]);
+        }).then(function (voted) {
+            // Check if this account address was added in voters list
+            assert(voted, "The voter was not marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "Vote was not incremented");
+        });
+    });
+    
+    it('Invalid candidateId cannot be voted', function() {
+        return Election.deployed().then( function(instance) {
+            electionInstance = instance;
+            return electionInstance.vote(99, { from : accounts[6] })
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, "Invalid candidateId was allowed to vote");
+            return electionInstance.candidates(1);
+        }).then(function (candidate1) {
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "candidate 1 was wrongly voted");
+            return electionInstance.candidates(2);            
+        }).then(function (candidate2) {
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 0, "candidate 2 was wrongly voted");
+        });
+    });
+
+    it('A voter cannot vote twice', function() {
+        Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            candidateId = 2;
+            electionInstance.vote(candidateId, { from : accounts[7]});
+            return electionInstance.candidates(candidateId);
+        }).then( function (candidate) {
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "Candidate was not voted for the first time");
+            // Vote again
+            return electionInstance.vote(candidateId, { from : accounts[7]});
+        }).then(assert.fail).catch(function (error) {
+            assert(error.message.indexOf("revert") >= 0, "Voter was able to vote 2nd time");
+            return electionInstance.candidates(1);
+        }).then(function (candidate1) {
+            voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "Candiate 1 did not recieve 1 vote");
+            return electionInstance.candidates(2);
+        }).then(function (candidate2) {
+            voteCount = candidate2[2];
+            assert.equal(voteCount, 1, "Candiate 2 did not recieve 1 vote");
+        });
+    });
+    
 });
